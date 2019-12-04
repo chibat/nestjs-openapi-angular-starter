0b0d3dc15933b6bf21c1112815a2664f04b073f2
chibat <tomofumi.chiba@gmail.com>
2019-12-04 20:23:39 +0900

    -

--- backend/pregenerate-client.ts
+++ backend/pregenerate-client.ts
@@ -2,12 +2,11 @@ import * as fs from 'fs';
 import del from 'del';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from '../src/app.module';
+import { AppModule } from './src/app.module';
 
 NestFactory.create(AppModule).then(app => {
   const options = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
   fs.writeFileSync('spec.json', JSON.stringify(document), 'UTF-8');
-  del(['../frontend/src/app/client'], {force: true});
+  del(['./frontend/src/app/client'], {force: true});
 });
